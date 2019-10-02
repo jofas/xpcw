3aebb6515159918a4b82cca834b7d91532f733c6
jofas <jonas@fc-web.de>
2019-10-02 15:15:26 +0100

    .

--- task2/reducer.py
+++ task2/reducer.py
@@ -2,39 +2,52 @@
 import sys
 
 class MovieProcessor:
-    def __init__(self):
+    def __init__(self, len_basic, len_rating, processor):
         self.basic  = []
         self.rating = []
 
+        self.len_basic  = len_basic
+        self.len_rating = len_rating
+
+        self.processor = processor
+
     def process(self, line):
-        if len(line) == 2 and len(self.rating) == 3:
+        if len(line) == self.len_basic and \
+                len(self.rating) == self.len_rating:
+
             if line[0] == self.rating[0]:
-                self.process_movie(line, self.rating)
+                self.processor(line, self.rating)
                 self.basic = []
             else:
                 self.basic = line
             self.rating = []
 
-        elif len(line) == 3 and len(self.basic) == 2:
+        elif len(line) == self.len_rating and \
+                len(self.basic) == self.len_basic:
+
             if line[0] == self.basic[0]:
-                self.process_movie(self.basic, line)
+                self.processor(self.basic, line)
                 self.rating = []
             else:
                 self.rating = line
             self.basic = []
 
-        elif len(line) == 2 and len(self.rating) == 0:
+        elif len(line) == self.len_basic and \
+                len(self.rating) == 0:
+
             self.basic = line
 
-        elif len(line) == 3 and len(self.basic) == 0:
+        elif len(line) == self.len_rating and \
+                len(self.basic) == 0:
+
             self.rating = line
 
-    def process_movie(self, basic, rating):
-        if float(rating[1]) >= 7.5 \
-                and int(rating[2]) >= 500000:
-            print("%s" % basic[1])
+def processor(basic, rating):
+    if float(rating[1]) >= 7.5 \
+            and int(rating[2]) >= 500000:
+        print("%s" % basic[1])
 
-proc = MovieProcessor()
+proc = MovieProcessor(2, 3, processor)
 
 for raw_line in sys.stdin:
     line = raw_line.strip().split("\t")
