bb0909cfbda920f35c69b75b78e827157ef875bc
jofas <jonas@fc-web.de>
2019-10-02 17:13:05 +0100

    .

---
+++ task3/reducer2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import sys
+
+cur_decade = None
+cur_genre = None
+
+for raw_line in sys.stdin:
+    line = raw_line.strip().split("\t")
+
+    decade = int(line[0])
+    genre  = line[1]
+
+    if decade != cur_decade:
+        cur_decade = decade
+        cur_genre = None
+        continue
+
+    if genre != cur_genre:
+        print ("%s|%s|%s" % (decade, genre, line[3]))
+        cur_genre = genre
+    else:
+        continue
