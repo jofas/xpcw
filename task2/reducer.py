cab5058563eea6c459e66e3ad260d41d1a5f4985
jofas <jonas@fc-web.de>
2019-10-02 14:19:27 +0100

    .

--- task2/reducer.py
+++ task2/reducer.py
@@ -38,8 +38,4 @@ proc = MovieProcessor()
 
 for raw_line in sys.stdin:
     line = raw_line.strip().split("\t")
-
-    if len(line) == 1:
-        print("%s" % line[0])
-    else:
-        proc.process(line)
+    proc.process(line)
--- task2/run.sh
+++ task2/run.sh
@@ -8,9 +8,6 @@ hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.
   -input $IN_DIR/title.ratings.tsv \
   -output $OUT_DIR \
   -mapper mapper.py \
-  -combiner combiner.py \
   -reducer reducer.py \
   -file mapper.py \
-  -file combiner.py \
-  -file reducer.py \
-  -file util.py
+  -file reducer.py
