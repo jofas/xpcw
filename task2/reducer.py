d43ee464eb8cf66cca68c2b67bbe7278e194e465
jofas <jonas@fc-web.de>
2019-10-02 14:10:53 +0100

    .

--- task2/reducer.py
+++ task2/reducer.py
@@ -1,7 +1,38 @@
 #!/usr/bin/python
 import sys
 
-from util import MovieProcessor
+class MovieProcessor:
+    def __init__(self):
+        self.basic  = []
+        self.rating = []
+
+    def process(self, line):
+        if len(line) == 2 and len(self.rating) == 3:
+            if line[0] == self.rating[0]:
+                self.process_movie(line, self.rating)
+                self.basic = []
+            else:
+                self.basic = line
+            self.rating = []
+
+        elif len(line) == 3 and len(self.basic) == 2:
+            if line[0] == self.basic[0]:
+                self.process_movie(self.basic, line)
+                self.rating = []
+            else:
+                self.rating = line
+            self.basic = []
+
+        elif len(line) == 2 and len(self.rating) == 0:
+            self.basic = line
+
+        elif len(line) == 3 and len(self.basic) == 0:
+            self.rating = line
+
+    def process_movie(self, basic, rating):
+        if float(rating[1]) >= 7.5 \
+                and int(rating[2]) >= 500000:
+            print("%s" % basic[1])
 
 proc = MovieProcessor()
 
