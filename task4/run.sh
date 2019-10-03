462b6f85f8eeb21c02b4c4b986b52d46e9b38fbf
Jonas Faßbender <s2000901@lute.inf.ed.ac.uk>
2019-10-03 16:55:40 +0100

    .

--- task4/run.sh
+++ task4/run.sh
@@ -6,8 +6,9 @@ hdfs dfs -rm -r $TMP_DIR
 hdfs dfs -rm -r $OUT_DIR
 
 hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
-  -input $IN_DIR/title.basics.tsv \
+  -input $IN_DIR/title.crew.tsv \
   -input $IN_DIR/title.ratings.tsv \
+  -input $IN_DIR/name.basics.tsv \
   -output $TMP_DIR \
   -mapper mapper.py \
   -reducer reducer.py \
@@ -16,14 +17,13 @@ hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.
 
 hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
     -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
-    -D stream.num.map.output.key.fields=4 \
-    -D mapreduce.partition.keycomparator.options="-k1,1 -k2,2nr" \
-    -D mapreduce.partition.keypartitioner.options=-k1 \
+    -D stream.num.map.output.key.fields=3 \
+    -D mapreduce.partition.keycomparator.options=-k2,2nr \
+    -D mapreduce.job.reduces=1 \
   -input $TMP_DIR/* \
   -output $OUT_DIR \
   -mapper cat \
-  -reducer cat \
-  -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner
+  -reducer cat
 
 hdfs dfs -cat $OUT_DIR/*
 
