fdd47048a3f075a1250bd0b5716686cab9434969
jofas <jonas@fc-web.de>
2019-10-04 16:51:04 +0100

    .

--- task3/mapper.py
+++ task3/mapper.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python
 import sys
 
+def process_basic(line):
+    t_num        = line[0]
+    title_type   = line[1]
+    title        = line[2]
+    release_year = line[5]
+    genres       = line[8]
+
+    if "\\N" in [title_type, title, genres, release_year]:
+        return
+
+    release_year = int(release_year)
+
+    if title_type == "movie" and release_year >= 1900 \
+            and release_year <= 1999:
+
+        decade = (release_year - 1900) / 10
+
+        print("%s\t%s\t%s\t%d" % (t_num, title, genres, decade))
+
 for raw_line in sys.stdin:
     line = raw_line.strip().split("\t")
 
-    if len(line) > 3:
-        t_num        = line[0]
-        title_type   = line[1]
-        title        = line[2]
-        release_year = line[5]
-        genres       = line[8]
-
-        if "\\N" in [title_type, genres, release_year]:
-            continue
-
-        release_year = int(release_year)
-
-        if title_type == "movie" and release_year >= 1900 \
-                and release_year <= 1999:
-
-            decade = (release_year - 1900) / 10
-
-            print("%s\t%s\t%s\t%d" % (t_num, title, genres, decade))
-    else:
+    if len(line) == 3:
+        # rating
         print("%s\t%s" % tuple(line[:2]))
+    else:
+        process_basic(line)
+
--- task3/reducer.py
+++ task3/reducer.py
@@ -1,57 +1,27 @@
 #!/usr/bin/python
 import sys
 
-class MovieProcessor:
-    def __init__(self, len_basic, len_rating, processor):
-        self.basic  = []
-        self.rating = []
+cur_basic = None
+cur_rating = None
 
-        self.len_basic  = len_basic
-        self.len_rating = len_rating
+def process_cur():
+    if cur_basic != None and cur_rating != None:
+        if cur_basic[0] == cur_rating[0]:
+            genres = cur_basic[2].split(",")
 
-        self.processor = processor
-
-    def process(self, line):
-        if len(line) == self.len_basic and \
-                len(self.rating) == self.len_rating:
-
-            if line[0] == self.rating[0]:
-                self.processor(line, self.rating)
-                self.basic = []
-            else:
-                self.basic = line
-            self.rating = []
-
-        elif len(line) == self.len_rating and \
-                len(self.basic) == self.len_basic:
-
-            if line[0] == self.basic[0]:
-                self.processor(self.basic, line)
-                self.rating = []
-            else:
-                self.rating = line
-            self.basic = []
-
-        elif len(line) == self.len_basic and \
-                len(self.rating) == 0:
-
-            self.basic = line
-
-        elif len(line) == self.len_rating and \
-                len(self.basic) == 0:
-
-            self.rating = line
-
-def processor(basic, rating):
-    genres = basic[2].split(",")
-
-    for genre in genres:
-        print("%s\t%s\t%s\t%s" % (
-            basic[3], genre, rating[1], basic[1]
-        ))
-
-proc = MovieProcessor(4, 2, processor)
+            for genre in genres:
+                print("%s\t%s\t%s\t%s" % (
+                    cur_basic[3], genre, cur_rating[1], cur_basic[1]
+                ))
 
 for raw_line in sys.stdin:
     line = raw_line.strip().split("\t")
-    proc.process(line)
+
+    if len(line) == 2:
+        cur_rating = line
+    else:
+        cur_basic = line
+
+    process_cur()
+
+process_cur()
