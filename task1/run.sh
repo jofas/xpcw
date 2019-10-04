917bd41306ce1b7ce151396f57dab3040b63ba4a
jofas <jonas@fc-web.de>
2019-10-04 15:12:59 +0100

    .

--- task1/run.sh
+++ task1/run.sh
@@ -1,7 +1,7 @@
-OUT_DIR=/user/$USER/task1
+OUT_DIR=/user/$USER/assignment/task1
 IN_DIR=/data/small/imdb
 
-hadoop dfs -rm -r $OUT_DIR
+hdfs dfs -rm -r $OUT_DIR
 
 hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
   -input $IN_DIR/title.basics.tsv \
@@ -12,3 +12,9 @@ hadoop jar /opt/hadoop/hadoop-2.9.2/share/hadoop/tools/lib/hadoop-streaming-2.9.
   -file mapper.py \
   -file combiner.py \
   -file reducer.py
+
+hdfs dfs -cat $OUT_DIR/* | head -20 > output.out
+
+if [ "$1" == "-v"]; then
+  hdfs dfs -cat $OUT_DIR/*
+fi
