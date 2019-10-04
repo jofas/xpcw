66ec83e1958ae16ef798a20b15f728daa941ae55
jofas <jonas@fc-web.de>
2019-10-04 15:20:39 +0100

    .

--- task1/mapper.py
+++ task1/mapper.py
@@ -4,9 +4,13 @@ import sys
 for raw_line in sys.stdin:
     line = raw_line.strip().split("\t")
 
-    runtime, genres = line[-2], line[-1]
+    title_type = line[1]
+    runtime    = line[-2]
+    genres     = line[-1]
 
-    if runtime == "\\N" or genres == "\\N": continue
+    if runtime == "\\N" or genres == "\\N" \
+            or title_type != "movie":
+        continue
 
     for genre in genres.split(","):
         print("%s\t%s" % (genre, runtime))
